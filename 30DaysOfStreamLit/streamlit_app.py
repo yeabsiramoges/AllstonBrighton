b6cb020107425acec7bb8c37125a109f8380a2a1
Yeabsira Moges <yeabsiramoges@dhcp-10-29-69-236.dyn.MIT.EDU>
2023-04-23 01:47:09 -0400

    filled write page

--- 30DaysOfStreamLit/streamlit_app.py
+++ 30DaysOfStreamLit/streamlit_app.py
@@ -1,8 +1,45 @@
 import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
 
-st.header('st.button')
+# menu options
 
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
+options = ("Control Statement", "Write")
+add_sidebar = st.sidebar.selectbox("Menu Options", options)
+
+if add_sidebar == "Control Statement":
+    st.header('st.button')
+
+    if st.button("Say hello"):
+        st.write("Why hello there")
+    else:
+        st.write("Goodbye")
+
+elif add_sidebar == "Write":
+    #Text
+    st.write("st.write")
+
+    #Italics and emojis
+    st.write("Hello, *World!* :sunglasses:")
+
+    #Numbers
+    st.write(1234)
+
+    #Dataframes
+    df = pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+    })
+    st.write(df)
+
+    st.write("Below is a Dataframe: ", df, "Above is a dataframe.")
+
+    df2 = pd.DataFrame(
+        np.random.rand(200, 3),
+        columns=['a', 'b', 'c']
+    )
+    c = alt.Chart(df2).mark_circle().encode(
+        x = 'a', y = 'b', size='c', color = 'c', tooltip=['a', 'b', 'c']
+    )
+    st.write(c)
