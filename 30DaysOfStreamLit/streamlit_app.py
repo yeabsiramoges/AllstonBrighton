56f9411c4bde60031dd6d2628ec42c223ad40aff
Yeabsira Moges <yeabsiramoges@dhcp-10-29-69-236.dyn.MIT.EDU>
2023-04-23 04:09:39 -0400

    finished 30 Days of Streamlit

--- 30DaysOfStreamLit/streamlit_app.py
+++ 30DaysOfStreamLit/streamlit_app.py
@@ -9,6 +9,7 @@ st.set_page_config(layout="wide")
 
 # menu options
 
+#@st.cache_data
 options = (
     "Control Statement", 
     "Write", 
@@ -220,4 +221,34 @@ elif add_sidebar == "Form":
     st.header("1. Example of using with notation")
     st.subheader("Coffee Machine")
 
+    with st.form("my_form"):
+        st.subheader("**Order your coffee**")
+
+        #Input widgets
+        coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+        coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+        brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+        serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+        milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+        owncup_val = st.checkbox('Bring own cup')
+
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        st.markdown(f'''
+            ☕ You have ordered:
+            - Coffee bean: `{coffee_bean_val}`
+            - Coffee roast: `{coffee_roast_val}`
+            - Brewing: `{brewing_val}`
+            - Serving type: `{serving_type_val}`
+            - Milk: `{milk_val}`
+            - Bring own cup: `{owncup_val}`
+        ''')
+    else:
+        st.write("Place your order!")
     
+    form = st.form("my_form_2")
+    seleced_val = form.slider("Select a value")
+    form.form_submit_button("Submit")
+
+    st.write("Selected value: ", seleced_val)
