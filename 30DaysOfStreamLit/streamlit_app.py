090d6ca5c4b8d98e116b330fa9a08d0ff5a340e9
Yeabsira Moges <yeabsiramoges@dhcp-10-29-69-236.dyn.mit.edu>
2023-04-23 02:57:42 -0400

    customization

--- 30DaysOfStreamLit/streamlit_app.py
+++ 30DaysOfStreamLit/streamlit_app.py
@@ -16,7 +16,8 @@ options = (
     "Multi-Select",
     "Checkbox",
     "Profiling",
-    "Latex")
+    "Latex",
+    "Customization")
 add_sidebar = st.sidebar.selectbox("Menu Options", options)
 
 if add_sidebar == "Control Statement":
@@ -134,3 +135,19 @@ elif add_sidebar == "Latex":
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)
      ''')
+    
+elif add_sidebar == "Customization":
+    st.title('Customizing the theme of Streamlit apps')
+    st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+    st.code("""
+    [theme]
+    primaryColor="#F39C12"
+    backgroundColor="#2E86C1"
+    secondaryBackgroundColor="#AED6F1"
+    textColor="#FFFFFF"
+    font="monospace"
+    """)
+
+    number = st.sidebar.slider('Select a number:', 0, 10, 5)
+    st.write('Selected number from slider widget is:', number)
