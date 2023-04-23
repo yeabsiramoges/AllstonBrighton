f84e548159dd8a294482668ac81ed00bfa3f9d43
Yeabsira Moges <yeabsiramoges@dhcp-10-29-69-236.dyn.MIT.EDU>
2023-04-23 09:46:41 -0400

    created barebones visualization tool

--- allston_brighton.py
+++ allston_brighton.py
@@ -1,1 +1,58 @@
+# Libraries
 import streamlit as st
+
+# Page Setup
+st.set_page_config(layout="wide")
+
+# Load Data
+@st.cache_data
+def load_images():
+    images = {
+        "county_map": "images/pop.png",
+        "tract_maps": []
+    }
+
+    relative_file_path = "images/"
+    file_extension = ".png"
+
+    for year in range(2011, 2022):
+        images["tract_maps"].append(relative_file_path + str(year) + file_extension)
+    
+    return images
+
+# Sidebar Setup
+options = (
+    "County View",
+    "Yearly Tract Maps",
+)
+add_sidebar = st.sidebar.selectbox("Menu Options", options)
+
+# Sidebar Control Flow
+if add_sidebar == "County View":
+    st.title("County View")
+    county_map = load_images()["county_map"]
+
+    with st.expander("About"):
+        st.write("Display overall map of county present day.")
+    
+    my_bar = st.progress(0)
+    for percent_complete in range(100):
+        my_bar.progress(percent_complete + 1)
+    
+    st.image(county_map, width=750)
+
+if add_sidebar == "Yearly Tract Maps":
+    st.title("Yearly Tract Maps")
+    tract_maps = load_images()["tract_maps"]
+
+    with st.expander("About"):
+        st.write("Display yearly tract maps showing incremental changes.")
+    
+    my_bar = st.progress(0)
+    for percent_complete in range(100):
+        my_bar.progress(percent_complete + 1)
+    
+    map_selector = st.slider("Select your year: ", 2011, 2020)
+
+    st.image(tract_maps[map_selector-2010], width=750)
+    pass
